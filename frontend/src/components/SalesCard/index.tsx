b1d19b2670e25264328583043b1dea97fd019c16
gaaica <gabriel.aires.camargo@gmail.com>
2022-11-19 15:31:45 -0300

    implementada busca de vendedores por data

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,8 +17,12 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then(response => {setSales(response.data.content)});
-  }, []);
+
+    const mind = minDate.toISOString().slice(0, 10);
+    const maxd = maxDate.toISOString().slice(0, 10);
+
+    axios.get(`${BASE_URL}/sales?minDate=${mind}&maxDate=${maxd}`).then(response => {setSales(response.data.content)});
+  }, [minDate, maxDate]);
 
   return (
     <div className="main-card">
